0036103716f53a59fe2d4ad638dfb37eccf179ea
FlutLab.io <>
2021-08-06 06:30:16 +0000

    Autosave

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:helloworld/model/model_movie.dart';
 import 'package:helloworld/widget/carousel_slider.dart';
+import 'package:helloworld/widget/circle_slider.dart';
 
 class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
@@ -42,7 +43,10 @@ class _HomeScreenState extends State<HomeScreen> {
             CarouselImage(movies: movies),
             TopBar(),
           ],
-        )
+        ), //Stack
+        CircleSlider(
+          movies: movies,
+        ),
       ],
     );
 
---
+++ lib/widget/circle_slider.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:helloworld/model/model_movie.dart';
+
+class CircleSlider extends StatelessWidget {
+  final List<Movie> movies;
+  CircleSlider({this.movies});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(7),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text('미리보기'),
+          Container(
+            height: 120,
+            child: ListView(
+              scrollDirection: Axis.horizontal, //기본은 vertical
+              children: makeCircleImages(context, movies),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+List<Widget> makeCircleImages(BuildContext context, List<Movie> movies) {
+  List<Widget> results = [];
+  for (var i = 0; i < movies.length; i++) {
+    results.add(
+      InkWell(
+        //클릭 가능한 위젯
+        onTap: () {},
+        child: Container(
+          padding: EdgeInsets.only(right: 10),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: CircleAvatar(
+              backgroundImage: AssetImage('images/' + movies[i].poster),
+              radius: 48,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+  return results;
+}
