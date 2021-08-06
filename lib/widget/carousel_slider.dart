f4d60a0b8736aa57ec720b9f998a4b865db25cf4
FlutLab.io <>
2021-08-06 06:53:39 +0000

    Autosave

--- lib/widget/carousel_slider.dart
+++ lib/widget/carousel_slider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:helloworld/model/model_movie.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:helloworld/screen/detail_screen.dart';
 
 class CarouselImage extends StatefulWidget {
   final List<Movie> movies;
